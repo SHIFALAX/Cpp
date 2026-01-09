609ca84ddc68270fd6c85b6d0122aedc6452f48d
Shifa Laxmeshwar <shifalax786@gmail.com>
2026-01-09 15:26:47 +0100

    Add waitForNotification code which uses conditon variable and lamba fucntion

---
+++ Threads/waitForNotification.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+using namespace std;
+
+
+bool ready = false;
+mutex mtx;
+condition_variable cv;
+
+void worker()
+{
+	//Unique_lock can unlock and relock
+	unique_lock<mutex> lock(mtx);
+	
+	cout<<"Wait for notification"<<endl;
+	//Wait until ready is true, cv.wait() releases the lock and waits for a notification.When notified, it reacquires the lock.
+	cv.wait(lock, []{return ready;});
+	
+	cout<<"worker thread processing"<<endl;
+
+}
+
+
+int main()
+{	
+	//Create thread 
+	thread t(worker);
+	
+	//Simulate some work
+	this_thread::sleep_for(chrono::seconds(1));
+	//Enter critical section
+	{
+		//Lock to protec ready variable, no need to lock or unlock manualy.
+		lock_guard<mutex> lock(mtx);
+		ready = true;
+	}
+	
+	//notify worker thread
+	cout<<"Notify Worker thread"<<endl;
+	cv.notify_one();
+	
+	t.join();
+	return 0;
+	
+}
